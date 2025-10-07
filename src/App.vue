7f210ca1d3ce93e01db4635f3abb8af99b630fc4
Manuel <dev@psicologica.ai>
2025-10-07 16:42:40 +0200

    test commit, changes?

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Homme</RouterLink>
+        <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
